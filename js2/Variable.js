2c3092932dfeedb7ab41a2550159176a02e01e5b
Navi2006 <piyushsingh955583@gmail.com>
2023-10-08 09:14:04 +0530

    js series

---
+++ js2/Variable.js
@@ -0,0 +1,28 @@
+const accountId = 129888
+let accountEmail = "Niluu23@gmail.com"
+var accountPassword = "9174697883"
+
+accountCity = "Jaipur"
+
+let accountState; // undefined value
+
+// accountId = 3 //not allowed
+
+accountEmail = "kahdssdjk@gmail.com"
+accountPassword = "439288"
+accountCity = "Bengaluru"
+
+
+
+console.log(accountId);
+
+
+console.table([accountEmail,accountPassword,accountId,accountCity,accountState]);
+
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope.
+
+*/
---
+++ js2/data_types.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs ,not browser
+
+console.log(4+6) //code readability should be high
+
+ console.log("Naveen")
+
+
+//Data_types in javascript
+
+ let name = "Naveen Singh"
+ let age = 18
+ let isLoggedin = "false"
+ let State = ""
+
+// all primitive datatypes
+
+
+ // number => 2 to the power of 53 :range
+ // bigint
+ // string => ""
+ // boolean => false/true
+ /* null => Standalone Value (specially a type)
+    It is a representation of an EMPTY value.*/
+ // undefined => when value is not defined to a declare variable
+ // symbol => used to define uniqeness
+
+
+
+ //object
+
+
+ console.log(typeof "Naveen"); // name is a string datatype
+ console.log(typeof age); // age is a number datatype
+ console.log(typeof null); //null is a object datatype
+ console.log(typeof undefined); // undefined is type
