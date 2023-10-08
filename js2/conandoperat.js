4fe25b0edb6348b7ce5e187791c38a8c63df0040
Navi2006 <piyushsingh955583@gmail.com>
2023-10-08 12:11:04 +0530

    conversion of  datatypes

---
+++ js2/conandoperat.js
@@ -0,0 +1,38 @@
+let score = "Naveen"
+
+//console.log(typeof score);
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1; false => 0;
+
+
+let isLoggedin = "Naveen"
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true in boolean
+// 0 => false
+// "" => false
+// "naveen" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
